4daa9a35d96084c0d360b7fe35ee41016aeb44f9
PETER ABBOTT <abbotsayshi@gmail.com>
2024-01-10 15:13:48 +0000

    attempted path fix changing homepage to homePage

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
-import HomePage from "./pages/homepage";
+import HomePage from "./pages/homePage";
 import EventsPage from "./pages/eventspage";
 import UseStatePage from "./pages/useStatePage";
 import CondRendPage from "./pages/condRendPage";
