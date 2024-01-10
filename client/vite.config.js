eee4cc57857d178d05ff58f6eabf6ff990cafd28
PETER ABBOTT <abbotsayshi@gmail.com>
2024-01-10 15:18:56 +0000

    changed vite.config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    base: "/path/to/your/base/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve("main.jsx"),
+      },
+    },
   },
 });
