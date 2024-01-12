c2ceb5525033953adca6dea42b926f5dd18222b5
PETER ABBOTT <abbotsayshi@gmail.com>
2024-01-12 12:35:16 +0000

    added local storage

--- client/src/pages/cookieclicker.jsx
+++ client/src/pages/cookieclicker.jsx
@@ -1,30 +1,27 @@
 import { useState, useEffect } from "react";
 
 export default function CookieClicker() {
-  const [cookies, setCookies] = useState(0);
-  const [cookiesPerSecond, setCookiesPerSecond] = useState(1);
-  const gameState = {
-    // values…
-    cookies: cookiesPerSecond,
-    // cookiesValue: cookiesValue,
-  };
+  // regarding variables logged to local storage
+  const [cookies, setCookies] = useState(
+    localStorage.getItem("cookies")
+      ? parseInt(localStorage.getItem("cookies"), 10) // 10 is the base Radix
+      : 0
+  );
+
+  const [cookiesPerSecond, setCookiesPerSecond] = useState(
+    localStorage.getItem("cookiesPerSecond")
+      ? parseInt(localStorage.getItem("cookiesPerSecond"), 10) // 10 is the base Radix
+      : 1
+  );
+
+  // regarding variables NOT logeed to local storage
+  const upgradeOne = 10 * cookiesPerSecond;
 
   useEffect(() => {
-    // gets game state
-    const savedGameState = localStorage.getItem("gameState");
-    if (savedGameState) {
-      const gameState = JSON.parse(savedGameState);
-      //   remap values
-      //   cookiesValue = gameState.cookiesValue;
-    }
-
     // set interval function
     const cookieInterval = setInterval(() => {
       setCookies((currentCookies) => currentCookies + 1);
     }, 1000 / cookiesPerSecond);
-
-    // saves game state
-    localStorage.setItem("gameState", JSON.stringify(gameState));
 
     // cleand up the intervals
     return () => {
@@ -32,18 +29,41 @@ export default function CookieClicker() {
     };
   }, [cookiesPerSecond]);
 
+  // regarding local storage
+  // split into individual useEffects to mitigate side effects and refactorability.
+  useEffect(() => {
+    localStorage.setItem("cookies", JSON.stringify(cookies));
+  }, [cookies]);
+
+  useEffect(() => {
+    localStorage.setItem("cookiesPerSecond", JSON.stringify(cookiesPerSecond));
+  }, [cookiesPerSecond]);
+
+  // increments the value of cookies per second
   function IncreaseCookiesPerSecond() {
     setCookiesPerSecond(cookiesPerSecond + 1);
   }
 
+  // resets the game values
+  function ResetGame() {
+    setCookiesPerSecond(1);
+    setCookies(0);
+  }
+
+  useEffect(() => {
+    if (cookies === upgradeOne) {
+      console.log("you can buy an upgrade");
+    }
+  });
+
   return (
     <>
-      <h1>Cookeie Clicker</h1>
+      <h1>Cookie Clicker</h1>
       <p>Cookies: {cookies}</p>
       <p>Cookies per second: {cookiesPerSecond}</p>
       <button onClick={IncreaseCookiesPerSecond}>Buy Cookie</button>
+      <button onClick={ResetGame}>reset game</button>
+      <button disabled>buy mutliplier</button>
     </>
   );
 }
-
-// you can use a ternary opporator to see if something is hidden or not
