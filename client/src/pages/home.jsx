cb0f36efc3218affcefebb1085e1a1029023f730
PETER ABBOTT <abbotsayshi@gmail.com>
2024-01-12 10:27:03 +0000

    wrapped anchors in list and created cookieclicker

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -2,10 +2,27 @@ export default function HomePage() {
   console.log("You are on the homepage");
   return (
     <>
-      <p>Hello this is the hompage</p>
-      <a href="/eventspage">mouseEvents</a>
-      <a href="/usestatepage">useState</a>
-      <a href="/conditionalrendering">conditional rendering</a>
+      <h1>Peter Abbotts project directory</h1>
+      <ul>
+        <li>
+          <a href="/eventspage">mouseEvents</a>
+        </li>
+        <li>
+          <a href="/usestatepage">useState</a>
+        </li>
+        <li>
+          <a href="/conditionalrendering">conditional rendering</a>
+        </li>
+        <li>
+          <a href="/maps">maps</a>
+        </li>
+        <li>
+          <a href="/useeffect">useffects</a>
+        </li>
+        <li>
+          <a href="/cookieclicker">cookieclicker</a>
+        </li>
+      </ul>
     </>
   );
 }
