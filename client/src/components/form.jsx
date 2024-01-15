811673b33180e62b3fbf6754604c8aa200f4e309
PETER ABBOTT <abbotsayshi@gmail.com>
2024-01-15 20:46:23 +0000

    added for validation pt one

--- client/src/components/form.jsx
+++ client/src/components/form.jsx
@@ -1,8 +1,23 @@
-export default function SimpleForm({ formId, children, onSubmit, onChange }) {
+import ActionButton from "./button";
+
+export default function SimpleForm({
+  formId,
+  htmlfor,
+  labelText,
+  uniqueId,
+  value,
+  onChange,
+}) {
+  // prevents default page refresh for form
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
   return (
     <>
-      <form id={formId} onSubmit={onSubmit} onChange={onChange}>
-        {children}
+      <form onSubmit={handleSubmit}>
+        <label htmlFor={htmlfor}>{labelText}</label>
+        <input type="text" id={uniqueId} onChange={onChange} value={value} />
+        <ActionButton type={"submit"} text={"click me!"} />
       </form>
     </>
   );
--- client/src/pages/formvalidation.jsx
+++ client/src/pages/formvalidation.jsx
@@ -3,35 +3,27 @@
 
 // imports
 import { useState } from "react";
-import ActionButton from "../components/button";
 import SimpleForm from "../components/form";
-import TextInput from "../components/textinput";
 
 export default function FormValidation() {
   // useState for TextInput
   const [name, setName] = useState("");
 
-  // prevents default page refresh for form
-  function handleSubmit(event) {
-    event.preventDefault();
-
-    // handle value change using onChange
-    function handleInputChange(event) {
-      setName(event.target.value);
-    }
+  function handleInputChange(event) {
+    setName(event.target.value);
   }
+
   return (
     <>
       <h1>Form validation</h1>
-      <SimpleForm onSubmit={handleSubmit} onChange={handleInputChange}>
-        <TextInput
-          htmlfor={"name"}
-          labelText={"name"}
-          uniqueId={"userName"}
-          placeHolderText={"please enter username"}
-        />
-        <ActionButton type={"submit"} text={"submit"} />
-      </SimpleForm>
+      <SimpleForm
+        htmlfor={"htmlfor"}
+        labelText={"name"}
+        uniqueId={"name"}
+        value={name}
+        onChange={handleInputChange}
+      ></SimpleForm>
+      <p> the value is : {name}</p>
     </>
   );
 }
