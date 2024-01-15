93141e4c4ad16a43668205e68ae43c72cb72cccf
PETER ABBOTT <abbotsayshi@gmail.com>
2024-01-15 11:37:10 +0000

    refactored navigation to use links

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,31 +1,18 @@
+import { Link } from "react-router-dom";
+
 export default function HomePage() {
   console.log("You are on the homepage");
   return (
     <>
-      <h1>Peter Abbotts project directory</h1>
-      <ul>
-        <li>
-          <a href="/eventspage">mouseEvents</a>
-        </li>
-        <li>
-          <a href="/usestatepage">useState</a>
-        </li>
-        <li>
-          <a href="/conditionalrendering">conditional rendering</a>
-        </li>
-        <li>
-          <a href="/maps">maps</a>
-        </li>
-        <li>
-          <a href="/useeffect">useffects</a>
-        </li>
-        <li>
-          <a href="/cookieclicker">cookieclicker</a>
-        </li>
-        <li>
-          <a href="/translations">translations</a>
-        </li>
-      </ul>
+      <nav>
+        <Link to={"/eventspage"}>Mouse Events</Link>
+        <Link to={"/usestatepage"}>useState</Link>
+        <Link to={"/conditionalrendering"}>Conditional Rendering</Link>
+        <Link to={"/maps"}>Maps</Link>
+        <Link to={"/useeffect"}>useEffect</Link>
+        <Link to={"/cookieclicker"}>Bumble Boogaloo</Link>
+        <Link to={"/translations"}>Translations</Link>
+      </nav>
     </>
   );
 }
